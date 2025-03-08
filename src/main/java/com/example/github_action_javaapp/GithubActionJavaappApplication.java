84771451413f6ffcd2be687d998cb18c411435ba
ShrishailHugar <shrishailhugar@test.com>
2025-03-09 00:02:34 +0530

    code

---
+++ src/main/java/com/example/github_action_javaapp/GithubActionJavaappApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_action_javaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionJavaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionJavaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github_action_javaapp/GithubActionJavaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_action_javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionJavaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
